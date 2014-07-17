6eaebd5e6d02b67090da983cfc6498bee75f523d
Toby Ho <airportyh@gmail.com>
2014-07-17 15:43:56 -0400

    Also calculate browserifiability score in the worker.

--- bin/worker.js
+++ bin/worker.js
@@ -12,6 +12,7 @@ var dir = '/tmp/browserify-search'
 var getModuleInfo = require('../lib/npm/get_module_info')
 var searchInfo = require('../lib/npm/search_info')
 var easyFeatures = require('../lib/npm/easy_features')
+var browserifiability = require('../lib/browserifiability')
 var debug = require('debug')('worker')
 var rimraf = require('rimraf')
 pull.connect('tcp://' + ip + ':3000')
@@ -19,68 +20,80 @@ push.connect('tcp://' + ip + ':3001')
 
 db(function(err, db){
   if (err) return console.error(err.message)
-  var Modules = db.collection('modules2')
 
-  var q = async.queue(function(module, done){
-    processModule(module, done)
-  }, 4)
-
-  pull.on('message', function(msg){
-    q.push(msg.toString())
-  })
-
-  function processModule(module, done){
-    getModuleInfo(module, function(err, info){
-      if (err){
-        push.send(
-          JSON.stringify({
-            _id: module,
-            invalid: true
-          })
-        )
-        return done()
-      }
-
-      if (!isValid(info)){
-        push.send(
-          JSON.stringify({
-            _id: module,
-            invalid: true
-          })
-        )
-        return done()
-      }
-      var version = info['dist-tags'].latest
-      var search = searchInfo(info)
-      var features = easyFeatures(info)
-      testModule(module, dir, function(err, results){
-        if (err){
-          console.error(module, err.message)
-          finish()
-          return
-        }
-        push.send(
-          JSON.stringify({
-            _id: module, 
-            version: version,
-            search: search,
-            features: features,
-            testResults: results
-          })
-        )
-
-        finish()
-
-        function finish(){
-          rimraf(path.join(dir, module), function(){
-            done()
-          })
-        }
-      })
+  var TestSummary = db.collection('test_summary')
+  TestSummary.find().toArray(function(err, testSummary){
+    var q = async.queue(function(module, done){
+      processModule(module, testSummary, done)
+    }, 4)
+    
+    pull.on('message', function(msg){
+      q.push(msg.toString())
     })
-  }
+  })  
 
 })
+
+function processModule(module, testSummary, done){
+  getModuleInfo(module, function(err, info){
+    if (err){
+      push.send(
+        JSON.stringify({
+          _id: module,
+          invalid: true
+        })
+      )
+      return done()
+    }
+
+    if (!isValid(info)){
+      push.send(
+        JSON.stringify({
+          _id: module,
+          invalid: true
+        })
+      )
+      return done()
+    }
+    var version = info['dist-tags'].latest
+    var search = searchInfo(info)
+    var features = easyFeatures(info)
+    testModule(module, dir, function(err, results){
+      if (err){
+        console.error(module, err.message)
+        finish()
+        return
+      }
+
+      var results = {
+        _id: module, 
+        version: version,
+        search: search,
+        features: features,
+        testResults: results
+      }
+
+      browserifiability(results, testSummary, function(err, browserifiability){
+        if (err){
+          results.browserifiability = {
+            error: err.message
+          }
+        }else{
+          results.browserifiability = browserifiability
+        }
+
+        push.send(JSON.stringify(results))
+        finish()
+      })
+
+      function finish(){
+        rimraf(path.join(dir, module), function(){
+          done()
+        })
+      }
+    })
+  })
+}
 
 function isValid(pkg){
   var tags = pkg['dist-tags']
