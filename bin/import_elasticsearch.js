a55d49e0c0fc7e9f1b727ca7a254e9e7c83895bd
Toby Ho <airportyh@gmail.com>
2014-07-18 21:42:03 -0400

    Yeah.

---
+++ bin/import_elasticsearch.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+var request = require('superagent')
+var db = require('../lib/db')
+var async = require('async')
+require('colors')
+
+db(function(err, db){
+
+  var Modules = db.collection('modules')
+
+  var q = async.queue(function(module, done){
+    if (typeof module.search.readme !== 'string'){
+      module.search.readme = ''
+    }
+    request
+      .put('http://localhost:9200/browserify-search/module/' + module._id)
+      .send(JSON.stringify({
+        browserifiability: module.browserifiability,
+        search: module.search
+      }))
+      .end(function(err, reply){
+        if (err) process.stdout.write('.'.red)
+        else process.stdout.write('.'.green)
+        done()
+      })
+  }, 10)
+
+  Modules.find({browserifiability: {$gt: 0}}).each(function(err, module){
+    q.push(module)
+  })
+
+})
