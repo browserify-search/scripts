b3101061a2e195efc06c4f703bc4274fd77bca6d
Toby Ho <airportyh@gmail.com>
2014-10-28 22:33:31 -0400

    Write to CURRENT_SEQ.

--- bin/follower.js
+++ bin/follower.js
@@ -4,6 +4,7 @@ var follow = require('follow-registry')
 var testModuleAndSave = require('../lib/test_module_and_save')
 var db = require('../lib/db')
 var cmdLn = require('cmd-ln')
+var fs = require('fs')
 
 cmdLn(function(since){
   db(function(err, db){
@@ -15,11 +16,10 @@ cmdLn(function(since){
       registry: 'https://registry.npmjs.org/',
       handler: function(data, callback){
         var module = data.json._id
-        console.log('testing ' + module)
+        var seq = data.seq
         testModuleAndSave(db, module, function(err){
           if (err) console.error(err.message)
-          console.log('done processing ' + module)
-          callback()
+          fs.writeFile('CURRENT_SEQ', String(seq), callback)
         })
       }
     }
