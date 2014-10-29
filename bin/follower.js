7fd21674de69ff7466dfae0eda7f4006acfd3f00
Toby Ho <airportyh@gmail.com>
2014-10-29 14:33:07 -0400

    Broken processModule into lib/process_module.js and now follower.js uses it.

--- bin/follower.js
+++ bin/follower.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 var follow = require('follow-registry')
-var testModuleAndSave = require('../lib/test_module_and_save')
+var processModule = require('../lib/process_module')
 var db = require('../lib/db')
 var cmdLn = require('cmd-ln')
 var fs = require('fs')
@@ -10,21 +10,35 @@ cmdLn(function(since){
   db(function(err, db){
     if (err) return console.error(err.message)
 
-    var options = {
-      since: since || 0,
-      inactivity_ms: 3600000,
-      registry: 'https://registry.npmjs.org/',
-      handler: function(data, callback){
-        var module = data.json._id
-        var seq = data.seq
-        testModuleAndSave(db, module, function(err){
-          if (err) console.error(err.message)
-          fs.writeFile('CURRENT_SEQ', String(seq), callback)
-        })
-      }
-    }
+    var TestSummary = db.collection('test_summary')
+    var Modules = db.collection('modules')
+    TestSummary.find().toArray(function(err, testSummary){
 
-    follow(options)
+      var options = {
+        since: since || 0,
+        inactivity_ms: 3600000,
+        registry: 'https://registry.npmjs.org/',
+        handler: function(data, callback){
+          var module = data.json._id
+          var seq = data.seq
+          processModule(module, testSummary, function(err, results){
+            if (err) return callback(err)
+            Modules.update(
+              {_id: module},
+              results,
+              {upsert: true},
+              function(err){
+                if (err) return callback(err)
+                fs.writeFile('CURRENT_SEQ', String(seq), callback)
+              }
+            )
+          })
+        }
+      }
+    
+      follow(options)
+
+    })
 
   })
 })
--- bin/worker.js
+++ bin/worker.js
@@ -1,19 +1,9 @@
 #! /usr/bin/env node
 
-var async = require('async')
-var path = require('path')
 var config = require('../config.json')
-var testModule = require('../lib/test_module')
-var dir = '/tmp/browserify-search'
-var getModuleInfo = require('../lib/npm/get_module_info')
-var searchInfo = require('../lib/npm/search_info')
-var easyFeatures = require('../lib/npm/easy_features')
-var browserifiability = require('../lib/browserifiability')
-var debug = require('debug')('worker')
-var rimraf = require('rimraf')
+var processModule = require('../lib/process_module')
 var getIP = require('../lib/get_ip')
 var zmq = require('zmq')
-var extend = require('util')._extend
 var socket = zmq.socket('req')
 var ip = getIP()
 var concurrency = 1
@@ -59,81 +49,4 @@ function start(testSummary){
   })
 }
 
-function processModule(module, testSummary, done){
-  console.log('Processing ' + module)
-  var timeMeasurements = {}
-  var startAll, start, end
-  startAll = start = +new Date
-  getModuleInfo(module, function(err, info){
-    timeMeasurements.moduleInfo = new Date - start
-    if (err){
-      timeMeasurements.all = new Date - startAll
-      return done(null, {
-        _id: module,
-        invalid: true,
-        timeMeasurements: timeMeasurements
-      })
-    }
 
-    if (!isValid(info)){
-      timeMeasurements.all = new Date - startAll
-      return done(null, {
-        _id: module,
-        invalid: true,
-        timeMeasurements: timeMeasurements
-      })
-    }
-    var version = info['dist-tags'].latest
-    var search = searchInfo(info)
-    var features = easyFeatures(info)
-    testModule(module, dir, function(err, results, testTimeMeasurements){
-      extend(timeMeasurements, testTimeMeasurements)
-      if (err){
-        console.error(module, err.message)
-        finish()
-        return
-      }
-
-      var results = {
-        _id: module, 
-        version: version,
-        search: search,
-        features: features,
-        testResults: results,
-        timeMeasurements: timeMeasurements
-      }
-
-      start = +new Date
-      browserifiability(results, testSummary, function(err, browserifiability){
-        timeMeasurements.browserifiability = new Date - start
-        if (err){
-          results.browserifiability = {
-            error: err.message
-          }
-        }else{
-          results.browserifiability = browserifiability
-        }
-
-        finish()
-      })
-
-      function finish(){
-        start = +new Date
-        rimraf(path.join(dir, module), function(){
-          timeMeasurements.rimraf = new Date - start
-          timeMeasurements.all = new Date - startAll
-          done(null, results)
-        })
-      }
-    })
-  })
-}
-
-function isValid(pkg){
-  var tags = pkg['dist-tags']
-  if (!tags) return false
-  var latest = tags.latest
-  if (!latest) return false
-  if (!pkg.versions[latest]) return false
-  return true
-}
