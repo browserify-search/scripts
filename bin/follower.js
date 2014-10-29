f5c7bec46bd7aa77de48dc22c153713eb2f30004
Toby Ho <airportyh@gmail.com>
2014-10-28 22:19:24 -0400

    Added since param.

--- bin/follower.js
+++ bin/follower.js
@@ -3,25 +3,28 @@
 var follow = require('follow-registry')
 var testModuleAndSave = require('../lib/test_module_and_save')
 var db = require('../lib/db')
+var cmdLn = require('cmd-ln')
 
-db(function(err, db){
-  if (err) return console.error(err.message)
+cmdLn(function(since){
+  db(function(err, db){
+    if (err) return console.error(err.message)
 
-  var options = {
-    since: 541157,
-    inactivity_ms: 3600000,
-    registry: 'https://registry.npmjs.org/',
-    handler: function(data, callback){
-      var module = data.json._id
-      console.log('testing ' + module)
-      testModuleAndSave(db, module, function(err){
-        if (err) console.error(err.message)
-        console.log('done processing ' + module)
-        callback()
-      })
+    var options = {
+      since: since || 0,
+      inactivity_ms: 3600000,
+      registry: 'https://registry.npmjs.org/',
+      handler: function(data, callback){
+        var module = data.json._id
+        console.log('testing ' + module)
+        testModuleAndSave(db, module, function(err){
+          if (err) console.error(err.message)
+          console.log('done processing ' + module)
+          callback()
+        })
+      }
     }
-  }
 
-  follow(options)
+    follow(options)
 
+  })
 })
